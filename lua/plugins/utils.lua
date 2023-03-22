aebb676d99e9da54aa569215200ca3a97d06a9ea
Isak Samsten <isak@samsten.se>
2023-03-22 13:04:31 +0000

    Fix harpoon keys

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -4,27 +4,47 @@ return {
   {
     "ThePrimeagen/harpoon",
     event = "VeryLazy",
-    keys = function()
-      local ui = require("harpoon.ui")
-      local mark = require("harpoon.mark")
-      return {
-        { "<leader>k", ui.toggle_quick_menu, desc = "List marks" },
-        { "<leader>a", mark.add_file, desc = "Mark file" },
-        { "[§", ui.nav_prev, desc = "Previous mark" },
-        { "]§", ui.nav_next, desc = "Next mark" },
-        {
-          "§",
-          function()
-            if vim.v.count == 0 then
-              ui.nav_file(1)
-            else
-              ui.nav_file(vim.v.count)
-            end
-          end,
-          desc = "Go to mark",
-        },
-      }
-    end,
+    keys = {
+      {
+        "<leader>k",
+        function()
+          require("harpoon.ui").toggle_quick_menu()
+        end,
+        desc = "List marks",
+      },
+      {
+        "<leader>a",
+        function()
+          require("harpoon.mark").add_file()
+        end,
+        desc = "Mark file",
+      },
+      {
+        "[§",
+        function()
+          require("harpoon.ui").nav_prev()
+        end,
+        desc = "Previous mark",
+      },
+      {
+        "]§",
+        function()
+          require("harpoon.ui").nav_next()
+        end,
+        desc = "Next mark",
+      },
+      {
+        "§",
+        function()
+          if vim.v.count == 0 then
+            ui.nav_file(1)
+          else
+            ui.nav_file(vim.v.count)
+          end
+        end,
+        desc = "Go to mark",
+      },
+    },
   },
   -- {
   --   "TimUntersberger/neogit",
