85155e2ab2cb1b428462ccd8426ce94dcc030d97
Isak Samsten <isak@samsten.se>
2023-03-29 11:36:03 +0000

    Improve pyright performance

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -66,6 +66,7 @@ return {
             python = {
               analysis = {
                 autoSearchPaths = true,
+                diagnosticMode = "openFilesOnly",
                 useLibraryCodeForTypes = true,
                 typeCheckingMode = "off",
               },
