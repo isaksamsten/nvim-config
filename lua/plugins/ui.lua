3e396c7c7e9ff3d598ca21bcce813967d6adb32c
Isak Samsten <isak@samsten.se>
2023-04-13 12:17:57 +0000

    Improve lualine separators

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -218,14 +218,14 @@ return {
           theme = "auto",
           globalstatus = true,
           disabled_filetypes = { statusline = { "dashboard", "lazy", "alpha" } },
-          component_separators = { left = "│", right = "│" },
-          section_separators = { left = "", right = "" },
+          component_separators = { left = "", right = "" },
+          section_separators = { right = "", left = "" },
         },
         sections = {
           lualine_a = {
             {
               "mode",
-              fmt = function(mode, ctx)
+              fmt = function(mode)
                 return string.sub(mode, 0, 6)
               end,
             },
