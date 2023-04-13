bebef513bd622eae5ef17c6d4970127fec8899ab
Isak Samsten <isak@samsten.se>
2023-04-13 12:16:31 +0000

    Improve highlight of diagnostic and neotree tabs

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,43 @@
 return {
   {
+    "folke/tokyonight.nvim",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+      light_style = "day", -- The theme is used when the background is set to light
+      transparent = false, -- Enable this to disable setting the background color
+      terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+      styles = {
+        -- Style to be applied to different syntax groups
+        -- Value is any valid attr-list value for `:help nvim_set_hl`
+        comments = { italic = true },
+        keywords = { italic = true },
+        functions = {},
+        variables = {},
+        -- Background styles. Can be "dark", "transparent" or "normal"
+        sidebars = "dark", -- style for sidebars, see below
+        floats = "dark", -- style for floating windows
+      },
+      sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+      day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+      hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+      dim_inactive = false, -- dims inactive windows
+      lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+
+      --- You can override specific color groups to use other groups or a hex color
+      --- function will be called with a ColorScheme table
+      ---@param colors ColorScheme
+      on_colors = function(colors) end,
+
+      --- You can override specific highlights to use other groups or a hex color
+      --- function will be called with a Highlights and ColorScheme table
+      ---@param highlights Highlights
+      ---@param colors ColorScheme
+      on_highlights = function(highlights, colors) end,
+    },
+  },
+  {
     "olimorris/onedarkpro.nvim",
     opts = {
       styles = {
@@ -82,6 +120,11 @@ return {
         TelescopePreviewNormal = { bg = "${telescope_preview}" },
         TelescopePreviewBorder = { fg = "${telescope_preview}", bg = "${telescope_preview}" },
 
+        NeoTreeTabActive = { bg = "${telescope_preview}" },
+        NeoTreeTabSeparatorActive = { fg = "${telescope_preview}", bg = "${telescope_preview}" },
+        NeoTreeTabInactive = { bg = "${telescope_results}" },
+        NeoTreeTabSeparatorInactive = { fg = "${telescope_results}", bg = "${telescope_results}" },
+
         -- Cmp
         CmpItemAbbrMatch = { fg = "${blue}", style = "bold" },
         CmpItemAbbrMatchFuzzy = { fg = "${blue}", style = "underline" },
@@ -102,6 +145,11 @@ return {
 
         -- DAP UI
         DapUIBreakpointsCurrentLine = { fg = "${yellow}", style = "bold" },
+
+        DiagnosticUnderlineError = { sp = "${red}", style = "undercurl" },
+        DiagnosticUnderlineWarn = { sp = "${yellow}", style = "undercurl" },
+        DiagnosticUnderlineInfo = { sp = "${blue}", style = "undercurl" },
+        DiagnosticUnderlineHint = { sp = "${cyan}", style = "undercurl" },
       },
       options = {
         cursorline = true,
