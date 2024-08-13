a77935f0897ad51d85d3b3c961da604456b32411
Isak Samsten <isak@samsten.se>
2024-08-13 13:52:01 +0200

    Correct color detection in TMUX

--- init.lua
+++ init.lua
@@ -6,6 +6,10 @@ vim.cmd([[
   " let g:loaded_perl_provider = 0
 ]])
 
+if vim.env.TMUX ~= nil then
+  vim.loop.fs_write(2, "\27Ptmux;\27\27]11;?\7\27\\", -1, nil)
+end
+
 -- Disable python3 provider
 vim.cmd.colorscheme("dragon")
 require("config.options")
@@ -13,3 +17,4 @@ require("config.lazy")
 require("config.keymaps")
 require("config.autocmds")
 require("config.signs")
+
